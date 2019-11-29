ddad5afbc3b5c184eda82b504dcd7ba83fad2c5a
Carlos <cpac20.4@gmail.com>
2019-11-29 11:46:36 +0000

    Script melhorado

--- Tutorial1/Ex1/smoothfilters.m
+++ Tutorial1/Ex1/smoothfilters.m
@@ -1,25 +1,34 @@
 
-
-baboon = rgb2gray(imread('baboon.png'));
-castle = rgb2gray(imread('castle.png'));
-lena = rgb2gray(imread('lena.jpg'));
-
-
+%%%%%%%%%%%% parametros %%%%%%%%%%%%
 originalName = 'baboon';
-filteringDomain = 'spatial';
-smoothingType = 'gaussian';
-filterParams = [20,2];
-% outputName = originalName + '_' + filteringDomain + '_' + smoothingType + '_' + mat2str(filterParams) + '.png';
+fileName = 'baboon.png';
 
-[noisy,smoothed] = main_smoothfilters(castle,'gaussian',[0,0.01],filteringDomain,smoothingType,filterParams);
+noiseType = 'salt&pepper';
+noiseParams = 0.05;
 
-figure('Name','smoothed');
-imshow(smoothed);
+filteringDomain = 'frequency';
+smoothingType = 'butterworth';
+filterParams = [5,2,1.2];
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-imwrite(smoothed,'castle_spatial_gaussian_20_2.png');
-%imwrite(noisy,'castle_gaussian_0_0,01.png');
-%{
+% funcao:
+image = rgb2gray(imread(fileName)); % imagem convertida para grayscale
+[noisy,smoothed] = main_smoothfilters(image,noiseType,noiseParams,filteringDomain,smoothingType,filterParams);
+
+
+% output:
+d = '_';
+
+params = strrep(regexprep(num2str(filterParams),'\s+',d),'.',',');
+output = strcat(originalName,d,filteringDomain,d,smoothingType,d,params,'.png');
+imwrite(smoothed,output);
+
+params = strrep(regexprep(num2str(noiseParams),'\s+',d),'.',',');
+output = strcat(originalName,d,noiseType,d,params,'.png');
+imwrite(noisy,output);
+
+
+% show
 figure('Name','Resultado');
 subplot(1,2,1), imshow(noisy); title('noisy');
 subplot(1,2,2), imshow(smoothed); title('smoothed');
-%}
