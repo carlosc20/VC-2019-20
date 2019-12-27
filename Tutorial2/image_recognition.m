bddd4ff9ac27f7535eebe325e969f63b1c9eb7b2
Luís Macedo <luisjrsm@hotmail.com>
2019-12-27 11:45:48 +0000

    Updates nos inputs

--- Tutorial2/image_recognition.m
+++ Tutorial2/image_recognition.m
@@ -1,11 +1,12 @@
 
 %%%%%%%%%%%% parametros %%%%%%%%%%%%
 fileName = 'coins.jpg';
+noiseType = 'gaussian'; % gaussian ou salt-an-pepper
+noiseParameters = [0,0.01]; % parametros do noise. Se for Gaussian, [media, variancia]. Se for Sal & Pepper, [ densidade ].
 
-
+image = rgb2gray(imread(fileName)); % imagem convertida para grayscale
+image = double(image)/255; % Converte a imagem para doubles e os valores para o intervalo [0,1]
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % funcao:
-image = rgb2gray(imread(fileName)); % imagem convertida para grayscale
-image = double(image)/255;
-[ preProcI, segI, segNoisyI, SNR, sizeHist, coinDist ] = main_image_recognition( image );
+[ preProcI, segI, segNoisyI, SNR, sizeHist, coinDist ] = main_image_recognition( image, noiseType, noiseParameters );
